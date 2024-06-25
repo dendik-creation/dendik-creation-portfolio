e5f0f9495bc0e753f6422a8f789b950eed3c885c
dendik-creation <setiawandendik0205@gmail.com>
2024-06-25 22:28:00 +0700

    fix: Fix style info data contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 import React, { ChangeEvent, FormEvent, useState } from "react";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -11,22 +18,27 @@ import {
   SelectTrigger,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+
 import { ContactForm, ContactInfo } from "@/types/TypeContact";
+
 import {
   FaEnvelope,
   FaLinkedin,
   FaMapMarkedAlt,
-  FaPhoneAlt,
+  FaWhatsapp,
 } from "react-icons/fa";
+
 import { motion } from "framer-motion";
 import { SelectValue } from "@radix-ui/react-select";
-import { Send, Unplug } from "lucide-react";
+import { Send, Unplug, Github } from "lucide-react";
 import { toast } from "sonner";
 import { Toaster } from "@/components/ui/sonner";
+import { SiUpwork } from "react-icons/si";
+import { TbBrandFiverr } from "react-icons/tb";
 
 const contactInfo: ContactInfo = [
   {
-    icon: <FaPhoneAlt />,
+    icon: <FaWhatsapp />,
     title: "Phone",
     description: "(+62) 851 7974 2593",
     href: "https://wa.me/6285179742593?message=Holla",
@@ -38,16 +50,34 @@ const contactInfo: ContactInfo = [
     href: "mailto:creationdendik729@gmail.com",
   },
   {
+    icon: <FaMapMarkedAlt />,
+    title: "Address",
+    description: "Kudus, Central Java Indonesia",
+    href: "https://maps.app.goo.gl/3eNP98LpccbmvYw57",
+  },
+  {
     icon: <FaLinkedin />,
     title: "Linkedin",
     description: "Dendi' Setiawan",
     href: "https://linkedin.com/in/dendi-setiawan-165057263",
   },
   {
-    icon: <FaMapMarkedAlt />,
-    title: "Address",
-    description: "Kudus, Central Java Indonesia",
-    href: "https://maps.app.goo.gl/3eNP98LpccbmvYw57",
+    icon: <SiUpwork />,
+    title: "Upwork",
+    description: "Dendi' Setiawan",
+    href: "https://www.upwork.com/freelancers/~019a1d14923d5e4999?mp_source=share",
+  },
+  {
+    icon: <TbBrandFiverr />,
+    title: "Fiverr",
+    description: "Dendi' Setiawan",
+    href: "https://www.fiverr.com/dendik_creation",
+  },
+  {
+    icon: <Github />,
+    title: "Github",
+    description: "Dendi' Creation",
+    href: "https://github.com/dendik-creation",
   },
 ];
 
@@ -117,9 +147,20 @@ const Contact: React.FC = () => {
     >
       <div className="container mx-auto">
         <Toaster theme="light" />
-        <div className="flex flex-col xl:flex-row gap-[30px]">
+        <div className="flex flex-col-reverse xl:flex-row gap-[30px]">
           {/* Form */}
-          <div className="xl:w-[54%] order-2 xl:order-none">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{
+              opacity: 1,
+              transition: {
+                delay: 1,
+                duration: 0.4,
+                ease: "easeIn",
+              },
+            }}
+            className="xl:w-[54%] order-2 xl:order-none"
+          >
             <form
               onSubmit={handleSendMessage}
               className="flex flex-col gap-6 p-10 bg-[#27272c] overflow-hidden relative rounded-xl"
@@ -208,10 +249,10 @@ const Contact: React.FC = () => {
                 <div className="">Send Message</div>
               </Button>
             </form>
-          </div>
+          </motion.div>
           {/* Info */}
-          <div className="flex-1 flex items-center xl:items-start xl:item xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            <ul className="flex flex-col gap-10">
+          <div className="flex-1 flex items-center xl:items-start xl:item order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10 xl:flex-row xl:flex-wrap">
               {contactInfo?.map((item, index) => (
                 <li key={index} className="group overflow-hidden">
                   <motion.div
@@ -230,14 +271,24 @@ const Contact: React.FC = () => {
                       href={item?.href}
                       className="flex items-center gap-6"
                     >
-                      <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
-                        <div className="text-[28px]">{item?.icon}</div>
-                      </div>
-                      <div className="flex-1">
+                      <TooltipProvider delayDuration={100}>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <div className="w-[52px] h-[52px] relative xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                              <div className="text-[28px]">{item?.icon}</div>
+                              <div className="absolute -bottom-0 rounded-md left-0 w-full h-0 bg-gradient-to-t from-accent/40 to-transparent opacity-0 group-hover:opacity-100 transition-all group-hover:h-8"></div>
+                            </div>
+                          </TooltipTrigger>
+                          <TooltipContent side="bottom">
+                            <p>{item?.description}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                      <div className="flex-1 xl:hidden block">
                         <p className="text-white/60">{item.title}</p>
-                        <h3 className="text-xl group-hover:underline transition-all group-hover:underline-offset-4">
+                        <p className="text-xl group-hover:underline transition-all group-hover:underline-offset-4">
                           {item.description}
-                        </h3>
+                        </p>
                       </div>
                     </a>
                   </motion.div>

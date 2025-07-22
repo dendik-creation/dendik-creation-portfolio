6f0d85ef0f7262d4d3fd3b1cfccba5b6f95c8343
dendik-creation <setiawandendik0205@gmail.com>
2025-07-22 19:43:49 +0700

    [feat] : add redirects custom

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/krs-ti",
+        destination:
+          "https://www.canva.com/design/DAGt48z9CcI/OJv2gCSPpBJbcfMX8kspLA/edit?utm_content=DAGt48z9CcI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;

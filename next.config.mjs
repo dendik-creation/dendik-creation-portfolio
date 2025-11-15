69a445644571dcfd1959413f72a463f685ae73b1
dendik-creation <setiawandendik0205@gmail.com>
2025-11-16 00:17:58 +0700

    [feat] add redirect path

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
           "https://www.canva.com/design/DAGt48z9CcI/OJv2gCSPpBJbcfMX8kspLA/edit?utm_content=DAGt48z9CcI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
         permanent: true,
       },
+      {
+        source: "/figma-roti-nasywa",
+        destination:
+          "https://www.figma.com/design/tQctxnoL4XcUoaV2pc3KED/IUM-Roti-Nasywa?node-id=0-1&t=IxWPzr1OQYNjjPuL-1",
+        permanent: true,
+      },
     ];
   },
 };
